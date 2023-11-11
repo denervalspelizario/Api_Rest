c5cb70dbef29f683cf73feee02baa01458d09b3c
denervalspelizario <denervalspelizario@gmail.com>
2023-11-11 10:29:59 -0300

    update adicionando mode secure do jest a aplicação

--- test/app.test.js
+++ test/app.test.js
@@ -2,7 +2,9 @@ const request = require('supertest');
 
 const app = require('../src/app'); // importando app que contem a rota get
 
-test('Deve responder na raiz', () => request(app).get('/') // rota 3001/  do tipo GET
-  .then((res) => {
-    expect(res.status).toBe(200); // expectativa é que o app na requisição get o status seja 200
-  }));
+test('Deve responder na raiz', () => {
+  request(app).get('/') // rota 3001/  do tipo GET
+    .then((res) => {
+      expect(res.status).toBe(200); // expectativa é que o app na requisição get o status seja 200
+    });
+});
